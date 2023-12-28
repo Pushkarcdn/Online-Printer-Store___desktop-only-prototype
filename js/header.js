b21680369632d26df01641d1d8eabbe6dc8eb045
Pushkar <pushkarcdn@gmail.com>
2023-12-28 19:37:29 +0545

    nav dynamic final

---
+++ js/header.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Fetch the navigation bar content
+    fetch("header.html")
+        .then(response => response.text())
+        .then(data => {
+
+            // Insert the navigation bar into the specified container
+            document.getElementById("header").innerHTML = data;
+
+            // Run JavaScript code for the injected content
+            // handleNavbarEvents();
+        })
+        .catch(error => console.error("Error fetching navigation bar:", error));
+});
+
+// function handleNavbarEvents() {
+
+//     // add javascript for header here
+
+// }
--- js/navbar.js
+++ js/navbar.js
@@ -1,12 +1,10 @@
-// navbar.js
-
 document.addEventListener("DOMContentLoaded", function () {
     // Fetch the navigation bar content
     fetch("navbar.html")
         .then(response => response.text())
         .then(data => {
             // Insert the navigation bar into the specified container
-            document.getElementById("navbar-container").innerHTML = data;
+            document.getElementById("navbar").innerHTML = data;
 
             // Run JavaScript code for the injected content
             handleNavbarEvents();
@@ -16,34 +14,34 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function handleNavbarEvents() {
 
-document.querySelector('#nav-home-button').addEventListener('click', function () {
-    window.location.href = 'index.html';
-});
+    document.querySelector('#nav-home-button').addEventListener('click', function () {
+        window.location.href = 'index.html';
+    });
 
-document.querySelector('#nav-products-button').addEventListener('click', function () {
-    window.location.href = 'products.html';
-});
+    document.querySelector('#nav-products-button').addEventListener('click', function () {
+        window.location.href = 'products.html';
+    });
 
-document.querySelector('#nav-blog-button').addEventListener('click', function () {
-    window.location.href = 'blog.html';
-});
+    document.querySelector('#nav-blog-button').addEventListener('click', function () {
+        window.location.href = 'blog.html';
+    });
 
-document.querySelector('#nav-research-button').addEventListener('click', function () {
-    window.location.href = 'research.html';
-});
+    document.querySelector('#nav-research-button').addEventListener('click', function () {
+        window.location.href = 'research.html';
+    });
 
-document.querySelector('#nav-about-button').addEventListener('click', function () {
-    window.location.href = 'about.html';
-});
+    document.querySelector('#nav-about-button').addEventListener('click', function () {
+        window.location.href = 'about.html';
+    });
 
-document.querySelector('#nav-contact-button').addEventListener('click', function () {
-    window.location.href = 'contact.html';
-});
+    document.querySelector('#nav-contact-button').addEventListener('click', function () {
+        window.location.href = 'contact.html';
+    });
 
-var pageName = document.title;
-pageName = pageName.split(' ')[0];
-pageName = pageName.toLowerCase();
+    var pageName = document.title;
+    pageName = pageName.split(' ')[0];
+    pageName = pageName.toLowerCase();
 
-document.querySelector('#nav-' + pageName + '-button').classList.add('current-page');
+    document.querySelector('#nav-' + pageName + '-button').classList.add('current-page');
 
 }
