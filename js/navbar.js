0b5b2f6bf52ec4646c77e4df0a1ab836e3ab7bb2
Pushkar <pushkarcdn@gmail.com>
2023-12-30 11:58:23 +0545

    nav buttons foeach loop

--- js/navbar.js
+++ js/navbar.js
@@ -14,34 +14,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function handleNavbarEvents() {
 
-    document.querySelector('#nav-home-button').addEventListener('click', function () {
-        window.location.href = 'index.html';
-    });
-
-    document.querySelector('#nav-products-button').addEventListener('click', function () {
-        window.location.href = 'products.html';
-    });
-
-    document.querySelector('#nav-blog-button').addEventListener('click', function () {
-        window.location.href = 'blog.html';
-    });
-
-    document.querySelector('#nav-research-button').addEventListener('click', function () {
-        window.location.href = 'research.html';
-    });
-
-    document.querySelector('#nav-about-button').addEventListener('click', function () {
-        window.location.href = 'about.html';
-    });
-
-    document.querySelector('#nav-contact-button').addEventListener('click', function () {
-        window.location.href = 'contact.html';
-    });
-
     var pageName = document.title;
     pageName = pageName.split(' ')[0];
     pageName = pageName.toLowerCase();
 
     document.querySelector('#nav-' + pageName + '-button').classList.add('current-page');
+
+    document.querySelector('#nav-home-button').addEventListener('click', function () {
+        window.location.href = 'index.html';
+    });
+
+    // Assigning link to each buttons on navbar:
+
+    var arrOfNavButtons = ["products", "blog", "research", "about", "contact"];
+
+    arrOfNavButtons.forEach(function arrOfNavButtons(value) {
+        document.querySelector('#nav-' + value + '-button').addEventListener('click', function () {
+            window.location.href = value + '.html';
+        });
+    });
 
 }
